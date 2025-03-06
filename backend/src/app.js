deb5dfdec1ca59e8418ba8f93f6ceaccfb9f1177
JuanDuq <juan.duque@ux.technology>
2025-03-06 14:58:26 -0500

    se une el back con el front

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const config = require('./config');
@@ -12,6 +13,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// Permitir solicitudes desde el frontend
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 //configuracion
 app.set('port',config.app.port);
